393d704c50e8e83a2b6eddd7692c5d63d0811ee6
Preeti-Antil02 <preetiantil006@gmail.com>
2026-09-12 17:34:14 +0530

    feat(frontend-v2): export unified global states framework

---
+++ frontend-v2/src/components/states/index.ts
@@ -0,0 +1,5 @@
+export { LoadingState } from "./LoadingState";
+export { EmptyState } from "./EmptyState";
+export { PartialState } from "./PartialState";
+export { ErrorState } from "./ErrorState";
+export { NotAvailableState } from "./NotAvailableState";
